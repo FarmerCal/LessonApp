929770ccaf9ab6756c434ba56be1b1c2b690d51b
callum day ham <caldayham@gmail.com>
2021-06-03 22:49:00 -0700

    - add assets - added models - parse data

---
+++ LessonApp/Utilites/DataServices.swift
@@ -0,0 +1,8 @@
+//
+//  DataServices.swift
+//  LessonApp
+//
+//  Created by Callum Day Ham on 6/3/21.
+//
+
+import Foundation
--- LessonApp/ViewModels/ContentModel.swift
+++ LessonApp/ViewModels/ContentModel.swift
@@ -9,4 +9,42 @@ import Foundation
 
 class ContentModel: ObservableObject{
     
+    @Published var modules = [Module]()
+    var styleData: Data?
+    
+    init(){
+        
+        getLocalData()
+    }
+    
+    func getLocalData(){
+     
+        let jsonUrl = Bundle.main.url(forResource: "lessons", withExtension: "json")
+        
+        do{
+            
+        let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            self.modules = modules
+        }
+        catch{
+            print("error 1")
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do{
+            
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+            
+        }
+        catch{
+            print("error 2")
+        }
+    }
 }
