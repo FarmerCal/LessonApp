1959c10388a87761f461c106e7d23da4c471f439
callum day ham <caldayham@gmail.com>
2021-06-03 19:04:13 -0700

    Initial Commit

---
+++ LessonApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LessonApp
+//
+//  Created by Callum Day Ham on 6/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LessonApp/LessonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonAppApp.swift
+//  LessonApp
+//
+//  Created by Callum Day Ham on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct LessonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
